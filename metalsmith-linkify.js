43a1e8bd4d5a6221b4d0958c27e795aea1466d58
Jared Krinke <jared.krinke@gmail.com>
2021-10-21 13:05:29 -0700

    Add note

--- metalsmith-linkify.js
+++ metalsmith-linkify.js
@@ -1,6 +1,6 @@
 export default () => ((files, metalsmith, done) => {
     Object.keys(files).forEach(key => {
-        // TODO: Add absolute links as well?
+        // TODO: Add absolute links as well (linkAbsolute), and rename this one to linkFromRoot
         files[key].link = key
             .replace(/\\/g, "/") // Convert slashes...
             .replace(/\/index.html$/, ""); // Remove file name
