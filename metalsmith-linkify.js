aedbaa1ba09cc95cf5a9e277a37aed1c24188b98
Jared Krinke <jared.krinke@gmail.com>
2021-10-21 13:48:52 -0700

    Create both relative and absolute links

--- metalsmith-linkify.js
+++ metalsmith-linkify.js
@@ -1,9 +1,16 @@
 export default () => ((files, metalsmith, done) => {
     Object.keys(files).forEach(key => {
-        // TODO: Add absolute links as well (linkAbsolute), and rename this one to linkFromRoot
-        files[key].link = key
+        // Link relative to the site root
+        const file = files[key];
+        file.linkFromRoot = key
             .replace(/\\/g, "/") // Convert slashes...
             .replace(/\/index.html$/, ""); // Remove file name
+        
+        // Absolute link (if absolute link to site root provided)
+        const siteUrl = metalsmith?.metadata()?.site?.url;
+        if (siteUrl) {
+            file.linkAbsolute = siteUrl + (siteUrl.endsWith("/") ? "" : "/") + file.linkFromRoot;
+        }
     });
     done();
 });
